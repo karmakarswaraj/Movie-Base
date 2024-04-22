a1dd2a35a16622e8c99ef2a42c87c2adb1cf3690
karmakarswaraj <swarajkarmakar5@gmail.com>
2024-04-22 21:04:36 +0530

    Final commit

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -7,7 +7,7 @@ import "./explore.scss";
 
 import useFetch from "../../hooks/useFetch";
 import { fetchDataFromApi } from "../../utilitiy/api";
-import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper.jsx";
 import MovieCard from "../../components/movieCard/MovieCard";
 import Spinner from "../../components/spinner/Spinner";
 
